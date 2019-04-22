dafe3fc91a0263585654b78a481c75516b3b6fdc
титаренко <kizuneworld@gmail.com>
2019-04-22 12:42:02 +0300

    Updates

---
+++ precache-manifest.1afe105e72168ef83c8139cabb74501f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca360ec2479119351051",
+    "url": "/test-todo-list-react/static/js/runtime~main.7477e762.js"
+  },
+  {
+    "revision": "fc52dbb93c9ac0e07e51",
+    "url": "/test-todo-list-react/static/js/main.b6987bf6.chunk.js"
+  },
+  {
+    "revision": "64226e5183b875a2637e",
+    "url": "/test-todo-list-react/static/js/2.bd6dd1d8.chunk.js"
+  },
+  {
+    "revision": "fc52dbb93c9ac0e07e51",
+    "url": "/test-todo-list-react/static/css/main.8ae51aed.chunk.css"
+  },
+  {
+    "revision": "0a1e781e4a17e07f60aa437155d18927",
+    "url": "/test-todo-list-react/index.html"
+  }
+];
